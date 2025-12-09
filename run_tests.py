79c10aacae0221268e3ac8f3a2a4a60a4e832b1a
Josue Diaz <josuediaz8@gmail.com>
2025-12-09 19:14:30 -0300

    Se suma el reporte con fecha y hora de la ejecucion de los tests

--- run_tests.py
+++ run_tests.py
@@ -1,13 +1,19 @@
 import pytest
+import datetime
 
 # Lista de archivos de pruebas a ejecutar
 test_files = [
-    "tests/test_login.py",
-    "tests/test_inventory.py",
-    "tests/test_cart.py"
+ #   "tests/test_login.py",
+  #  "tests/test_inventory.py",
+    "tests/test_cart.py",
+    "tests/test_api.py"
 ]
 
-# Argumentos para ejecutar las pruebas: archivos + reporte HTML
-#pytest_args = test_files + ["--html=report.html","--self-contained-html","-v"]
+#Armo estructura de fecha para Reporte
+now = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") 
 
-pytest.main(test_files)
+# Argumentos para ejecutar las pruebas: archivos + reporte HTML
+pytest_args = test_files + [f"--html=report-{now}.html", "--self-contained-html", "-v"]
+
+
+pytest.main(pytest_args)
