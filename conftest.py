9e2f818ce9562cb5a4985720ae3dbc292e9a1b62
Josue Diaz <josuediaz8@gmail.com>
2025-12-09 15:49:59 -0300

    Pruebas del login realizadas

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+from selenium import webdriver
+from pages.login_page import LoginPage
+from selenium.webdriver.chrome.options import Options
+
+import pathlib
+from datetime import datetime
+import time
+
+
+@pytest.fixture
+def driver():
+    options = Options()
+    options.add_argument("--incognito")
+
+    driver = webdriver.Chrome(options=options)
+    yield driver
+    driver.quit()
+
+@pytest.fixture
+def login_in_driver(driver):
+    LoginPage(driver).abrir_pagina()
+    return driver
---
+++ tests/test_login.py
@@ -0,0 +1,21 @@
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+import pytest 
+import time
+
+from utils.datos import leer_csv_login
+from pages.login_page import LoginPage
+
+
+@pytest.mark.parametrize("usuario,password,debe_funcionar",leer_csv_login("datos/data_login.csv"))
+def test_login_validation(login_in_driver,usuario,password,debe_funcionar):
+    driver = login_in_driver
+    time.sleep(2)
+    if debe_funcionar == True:
+        assert "/inventory.html" in driver.current_url, "No se redirgio al inventario"
+    elif debe_funcionar == False:
+        mensaje_error = LoginPage(driver).obtener_error()
+        assert "Epic sadface" in mensaje_error, "el mensaje de error no se esta mostrando"
+
+
+
